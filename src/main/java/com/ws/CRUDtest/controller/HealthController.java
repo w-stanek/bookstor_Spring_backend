bf41bc42ad78871dfbb772873d8f4536346800c7
w-stanek <93320171+w-stanek@users.noreply.github.com>
2022-05-08 12:17:23 +0200

    removing unnecessary stuff

--- src/main/java/com/ws/CRUDtest/controller/HealthController.java
+++ src/main/java/com/ws/CRUDtest/controller/HealthController.java
@@ -1,9 +1,5 @@
 package com.ws.CRUDtest.controller;
-
-import java.sql.SQLException;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
